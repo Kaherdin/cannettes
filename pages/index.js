14f89c0942eb8e30ff992acbf1f3bb5d5d94afaa
Kaherdin <aurelien.borst@gmail.com>
2021-09-08 15:57:10 +0200

    Add post url

--- pages/index.js
+++ pages/index.js
@@ -1,30 +1,43 @@
-import Head from 'next/head'
-import Image from 'next/image'
-import styles from '../styles/Home.module.css'
+import Head from "next/head";
+import Image from "next/image";
+import styles from "../styles/Home.module.css";
+import axios from "axios";
+import oauth from "axios-oauth-client";
 
 export default function Home() {
   //npm install --save axios-oauth-client axios-token-interceptor axios
 
-  const axios = require('axios');
-  const oauth = require('axios-oauth-client');
+  // Helper to make Authenficated POST requests to Strapi with axios
+  async function postAxiosAPI(path, data, userToken) {
+    const requestUrl = getStrapiURL(path);
+    const response = await axios.post(requestUrl, data, {
+      headers: userToken && { Authorization: `Bearer ${userToken}` },
+    });
+    return response;
+  }
+
   const getAuthorizationCode = oauth.client(axios.create(), {
-    url: 'https://api.intra.42.fr/oauth/authorize',
-    grant_type: 'authorization_code',
-    client_id: 'f5b5d42fac237f01e60e8141cfb7cad56c8a99d5a4128356f1846978f3ad7383',
-    client_secret: '1707f43fe58bab0bb4c4413a61604804116c4b91a3e2513842412af72a4d14eb',
-    redirect_uri: 'https://api.intra.42.fr/oauth/authorize?client_id=f5b5d42fac237f01e60e8141cfb7cad56c8a99d5a4128356f1846978f3ad7383&redirect_uri=https%3A%2F%2Fcannettes.vercel.app%2F&response_type=code',
-/*     code: '...',
-    scope: 'baz', */
+    url: "https://api.intra.42.fr/oauth/authorize",
+    grant_type: "authorization_code",
+    client_id:
+      "f5b5d42fac237f01e60e8141cfb7cad56c8a99d5a4128356f1846978f3ad7383",
+    client_secret:
+      "1707f43fe58bab0bb4c4413a61604804116c4b91a3e2513842412af72a4d14eb",
+    redirect_uri:
+      "https://api.intra.42.fr/oauth/authorize?client_id=f5b5d42fac237f01e60e8141cfb7cad56c8a99d5a4128356f1846978f3ad7383&redirect_uri=https%3A%2F%2Fcannettes.vercel.app%2F&response_type=code",
+    // code: '...',
+    scope: "public",
   });
 
-//  https://api.intra.42.fr/oauth/authorize?client_id=f5b5d42fac237f01e60e8141cfb7cad56c8a99d5a4128356f1846978f3ad7383&redirect_uri=https%3A%2F%2Fcannettes.vercel.app%2F&response_type=code
+  //Post request working with postman :
+  // https://api.intra.42.fr/oauth/token?client_id=f5b5d42fac237f01e60e8141cfb7cad56c8a99d5a4128356f1846978f3ad7383&client_secret=1707f43fe58bab0bb4c4413a61604804116c4b91a3e2513842412af72a4d14eb&grant_type=client_credentials
 
+  //  https://api.intra.42.fr/oauth/authorize?client_id=f5b5d42fac237f01e60e8141cfb7cad56c8a99d5a4128356f1846978f3ad7383&redirect_uri=https%3A%2F%2Fcannettes.vercel.app%2F&response_type=code
 
   //https://stackoverflow.com/questions/55435471/how-to-convert-a-curl-command-to-an-axios-call-in-react-get-put-post
   //curl -X POST --data "grant_type=client_credentials&client_id=MY_AWESOME_UID&client_secret=MY_AWESOME_SECRET" https://api.intra.42.fr/oauth/token
 
-  
-  const auth = await getAuthorizationCode(); // => { "access_token": "...", "expires_in": 900, ... }
+  //const auth = getAuthorizationCode(); // => { "access_token": "...", "expires_in": 900, ... }
 
   return (
     <div className={styles.container}>
@@ -40,7 +53,7 @@ export default function Home() {
         </h1>
 
         <p className={styles.description}>
-          Get started by editing{' '}
+          Get started by editing{" "}
           <code className={styles.code}>pages/index.js</code>
         </p>
 
@@ -81,12 +94,12 @@ export default function Home() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Powered by{' '}
+          Powered by{" "}
           <span className={styles.logo}>
             <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
           </span>
         </a>
       </footer>
     </div>
-  )
+  );
 }
